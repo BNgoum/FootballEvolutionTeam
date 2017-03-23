6506082ea2c2510e303b8096394e6213bf142b11
bngoum <benjamin.ngoum@gmail.com>
2017-03-23 16:01:41 +0100

    mise a jour database

--- app/models/joueur.rb
+++ app/models/joueur.rb
@@ -2,9 +2,9 @@ class Joueur < ActiveRecord::Base
 	# validates :name, :club, :nationalite, :poste, :statsgenerale, :pac, :dri, :tir, :def, :pas, :phy, :qualite, presence: { message: ': ce champs ne doit pas être vide !'}
 	# belongs_to :user
 	# belongs_to :equipe
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	has_attached_file :image
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
+	belongs_to :equipe
 	has_many :equipe_persos
 
 	
